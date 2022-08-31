22f2a13435384743472db7fa5a67f016e10ae4de
Andrew Chu <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-08-31 01:05:40 -0400

    Template for CSS and JS tutorial files - No new top navbar - XML instead of text

--- assets/template.js
+++ assets/template.js
@@ -1,106 +1,49 @@
-/*
- * This is a questionable way to make templates for the tutorial pages.
- */
-
-const defaultTemplate = {
-	head: `    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- stylesheets -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/navigation.css">
-	<link rel="stylesheet" href="/assets/tutorials.css">
-
-    <!-- icon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-tilecolor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">`,
-	topBar: `    <nav class="navbar fixed-top" id="top-navbar">
-        <div class="container px-5">
-            <a href="/">
-                <img src="/assets/site-logo.png">
-            </a>
-            <h4>RHHS Web Development</h4>
-        </div>
-    </nav>`,
-	sideBar: ``
-};
-
-const htmlTemplate = {
-	head: defaultTemplate.head,
-	topBar: defaultTemplate.topBar, 
-	sideBar: `<nav id="side-navbar">
-			<a href="/tutorials/html">HTML</a></li>
-			<div>
-				<header>HTML Basics</header>
-				<ul>
-					<li><a href="/tutorials/html/lesson-1">Doctype</a></li>
-					<li><a href="/tutorials/html/lesson-2">Elements</a></li>
-					<li><a href="/tutorials/html/lesson-3">Text Formatting</a></li>
-					<li><a href="/tutorials/html/lesson-4">Lists</a></li>
-					<li><a href="/tutorials/html/lesson-5">Attributes</a></li>
-					<li><a href="/tutorials/html/lesson-6">Commments</a></li>
-				</ul>
-			</div>
-			<div>
-				<header>HTML Advanced</header>
-				<ul>
-					<li><a href="/tutorials/html/lesson-7">Content Division</a></li>
-					<li><a href="/tutorials/html/lesson-8">Tables</a></li>
-					<li><a href="/tutorials/html/lesson-9">Forms</a></li>
-					<li><a href="/tutorials/html/lesson-10">Head Element</a></li>
-				</ul>
-			</div>
-		</nav>`
-};
-
-const cssTemplate = {
-	head: defaultTemplate.head,
-	topBar: defaultTemplate.topBar,
-	sideBar: ``
-};
-
-const jsTemplate = {
-	head: defaultTemplate.head,
-	topBar: defaultTemplate.topBar,
-	sideBar: ``
-};
-
 // Adds the header, top navbar, and side navbar to tutorial pages.
 function useTemplate(type) {
-	let template;
+	type = type.toLowerCase();
 
-	type = type.toUpperCase();
-	if (type === "HTML") {
-		template = htmlTemplate;
-	} else if (type === "CSS") {
-		template = cssTemplate;
-	} else if (type === "JS") {
-		template = jsTemplate;
-	} else {
-		console.log(`Error: unrecognized template type: ($type)`);
-		return;
+	let valid = new Array("html", "css", "js").includes(type);
+	if (!valid) {
+		throw `Error: invalid tutorial type (${type})`;
 	}
 
-	let headElem = document.getElementsByTagName("head").item(0);
-	if (headElem) {
-		headElem.innerHTML = template.head + headElem.innerHTML;
-	}
-	
-	let topBarElem = document.getElementById("top-bar");
-	if (topBarElem) {
-		topBarElem.innerHTML = template.topBar + topBarElem.innerHTML;
-	}
+	loadDoc(`/assets/${type}.xml`, function(xhttp) {
+		const xmlDoc = xhttp.responseXML;
 
-	let sideBarElem = document.getElementById("side-bar");
-	if (sideBarElem) {
-		sideBarElem.innerHTML = template.sideBar + sideBarElem.innerHTML;
-	}
+		let headElem = document.getElementsByTagName("head").item(0);
+		let headData = xmlDoc.getElementsByTagName("head").item(0);
+		if (headElem && headData) {
+			headElem.innerHTML =  headData.innerHTML + headElem.innerHTML;
+		}
+
+		let topBarElem = document.getElementById("top-bar");
+		let topBarData = xmlDoc.getElementById("top-bar");
+		if (topBarElem && topBarData) {
+			topBarElem.innerHTML = topBarData.innerHTML + topBarElem.innerHTML;
+		}
+
+		let sideBarElem = document.getElementById("side-bar");
+		let sideBarData = xmlDoc.getElementById("side-bar");
+		if (sideBarElem && sideBarData) {
+			sideBarElem.innerHTML = sideBarData.innerHTML + sideBarElem.innerHTML;
+		}
+	});
+}
+
+// Performs callback function on AJAX'd data.
+function loadDoc(url, callback) {
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.onload = function() {
+		if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
+			callback(this);
+		} else {
+			throw `Error: there was a problem with the XML HTTP request to ${url}.`;
+		}
+	};
+
+	xhttp.open("GET", url, false); // must be synchronous for selectActiveLesson()
+	xhttp.send();
 }
 
 // Add the 'active' class to relevant html elements in the navbar.
@@ -108,13 +51,13 @@ function selectActiveLesson() {
 	let pathName = window.location.pathname; // e.g., '/tutorials/html/lesson-1/'
 	let href = pathName.substr(0, pathName.length - 1); // '/tutorials/html/lesson-1'
 
-	let activeElems = document.querySelectorAll(`[href='${href}']`);
+	let activeElems = document.querySelectorAll(`a[href='${href}']`);
 
 	if (activeElems) {
 		for (const elem of activeElems) {
 			elem.classList.add('active');
 		}
 	} else {
-		console.log("something is up");
+		throw "Error: no .active elements";
 	}
 }
